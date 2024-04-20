cb8eb5613eb241b4fd37d05d4dec41f326762d0d
accio-rajitha <rajithasabhavath2019@gmail.com>
2024-04-20 15:52:29 +0530

    Selector is added

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,32 @@
+// Selection.js
+import React, { useState } from 'react';
+
+const Selection = ({ selectedColor }) => {
+  const [background, setBackground] = useState(null);
+
+  return (
+    <div 
+      className="fix-box" 
+      style={{ 
+        background, 
+        width: '200px', 
+        height: '200px', 
+        display: 'inline-block', 
+        border: '1px solid white',
+        
+        color:'white',
+        textAlign:'center'
+        
+      }} 
+      onClick={() => setBackground(selectedColor)}
+    >
+        <div style={{ paddingTop: '10px' }}> 
+        Selection
+      </div>
+      
+    </div>
+  );
+};
+
+export default Selection;
 
